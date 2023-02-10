95f551792d5b294da7cf7793e63af1f528545380
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-02-10 12:21:20 +0100

    primo commit progetto calcolatrice

---
+++ m1/s2/g5/progetto/js/script.js
@@ -0,0 +1,14 @@
+function aggiungiSimbolo(elemento) {
+    let simbolo = elemento.getAttribute('data-simbolo');
+    let display = document.querySelector('#display');
+    display.value += simbolo;
+}
+function totale() {
+    let display = document.querySelector("#display");
+    display.value = eval(display.value);
+}
+function reset() {
+    let display = document.querySelector("#display");
+    display.value = '';
+    console.log(display);
+}
