c9fa0437d32878eace7853f2afbf161876865cb2
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-02-10 15:58:07 +0100

    progetto calcolatrice

--- m1/s2/g5/progetto/js/script.js
+++ m1/s2/g5/progetto/js/script.js
@@ -1,14 +1,51 @@
+let a,b;
+let risultato;
+
+const calcolatrice = {
+    somma: function(a,b) {
+        return a + b;
+    },
+    sottrazione: function(a,b) {
+        return a - b;
+    },
+    divisione:function(a,b) {
+        return a / b;
+    },
+    moltiplicazione:function(a,b) {
+        return a * b;
+    }
+};
 function aggiungiSimbolo(elemento) {
     let simbolo = elemento.getAttribute('data-simbolo');
     let display = document.querySelector('#display');
     display.value += simbolo;
-}
-function totale() {
+};
+/*function totale() {
     let display = document.querySelector("#display");
     display.value = eval(display.value);
-}
+}*/
+function totale() {
+    let display = document.querySelector("#display");
+    console.log(display.value);
+    let numeri = display.value.split(/[!\s+-/*X]+/);
+    a = Number(numeri[0]);
+    b = Number(numeri[1]);
+
+    let operazione = String(display.value);
+    if(operazione.includes('+')){
+        risultato=calcolatrice.somma(a,b);
+    }else if(operazione.includes('-')){
+        risultato=calcolatrice.sottrazione(a,b);
+    }else if(operazione.includes('X')){
+        risultato=calcolatrice.moltiplicazione(a,b);
+    }else if(operazione.includes('/')){
+        risultato=calcolatrice.divisione(a,b);
+    }
+    display.value =risultato;
+};
 function reset() {
     let display = document.querySelector("#display");
     display.value = '';
     console.log(display);
-}
+};
+
