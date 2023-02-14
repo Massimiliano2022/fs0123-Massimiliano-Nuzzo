55cfc15bca6d591b0ef224422439cde5f832b5a9
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-02-14 16:52:04 +0100

    esercizi

---
+++ m1/s3/g2/esercizio/js/D7.js
@@ -0,0 +1,369 @@
+/* ESERCIZIO 1
+  Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
+  prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
+*/
+
+function concatenaStringhe(str1, str2) {
+  let result;
+
+  str1 = str1.substring(0, 2);
+  str2 = str2.slice(-3);
+
+  result = str1.toUpperCase() + str2.toUpperCase();
+  return result;
+
+}
+console.log(concatenaStringhe('ciao', 'benvenuto'));
+
+/* ESERCIZIO 2
+  Scrivi una funzione per creare un array di soli valori DISPARI da 1 a 100.
+ */
+
+function creaArrayNumDispari() {
+  let arrNumDispari = [];
+  for (i = 0; i <= 100; i++) {
+
+    if (i % 2 != 0) {
+      arrNumDispari.push(i);
+    }
+  }
+  return arrNumDispari;
+}
+
+console.log(creaArrayNumDispari());
+
+/* ESERCIZIO 3
+  Scrivi una funzione per creare un array di soli valori multipli di 5, da 1 a 100.
+ */
+
+function creaArrayMult5() {
+
+  let arr = [];
+
+  for (i = 0; i <= 100; i++) {
+
+    if (i % 5 == 0) {
+
+      arr.push(i);
+
+    }
+  }
+  return arr;
+}
+
+console.log(creaArrayMult5());
+
+/* ESERCIZIO 4
+  Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
+ */
+
+function creaArrRandom() {
+  let arr = [];
+  let numRandom;
+  for (i = 0; i < 10; i++) {
+
+    numRandom = Math.floor(Math.random() * 100);
+
+    arr.push(numRandom);
+
+  }
+
+  return arr;
+
+}
+console.log(creaArrRandom());
+
+/* ESERCIZIO 5
+  Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici.
+ */
+
+function ottieniNumPari(arr) {
+  let arrayPari = [];
+  for (let n of arr) {
+    if (n % 2 == 0) {
+      arrayPari.push(n);
+    }
+  }
+  return arrayPari;
+}
+console.log(ottieniNumPari([1, 2, 6, 3, 5, 7, 9, 10]));
+
+/* ESERCIZIO 6
+  Scrivi una funzione per sommare a catena i numeri contenuti in un array.
+ */
+
+function sommaNumeriArray(arr) {
+  let totale = new Number();
+  let numero = new Number();
+  for (let n of arr) {
+    numero = n;
+    totale += numero;
+  }
+  return totale;
+}
+console.log(sommaNumeriArray([1, 2, 3, 4, 5]));
+
+/* ESERCIZIO 7
+  Scrivi una funzione per incrementare di 1 tutti i valori numerici in un array.
+*/
+function incrementaDiUno(arr) {
+  let numero = new Number();
+  let nuovoArray = [];
+  for (let n of arr) {
+
+    numero = n + 1;
+    nuovoArray.push(numero);
+  }
+  return nuovoArray;
+}
+console.log(incrementaDiUno([1, 2, 3, 4, 5]));
+
+/* ESERCIZIO 8
+  Sostituisci ogni stringa contenuta in un array con un numero rappresentante la sua lunghezza.
+  es.: ["EPICODE", "is", "great"] => [7, 2, 5]
+*/
+/*function stringToLunghezza(array) {
+  let numeroCaratteri = new Number();
+  let nuovoArray = [];
+
+  for (let stringa of array) {
+
+    numeroCaratteri = stringa.length;
+    nuovoArray.push(numeroCaratteri);
+
+  }
+  return nuovoArray;
+}*/
+
+function sostituisciConLunghezza(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i].length;
+  }
+  return array;
+}
+
+console.log(sostituisciConLunghezza(["EPICODE", "is", "great"]));
+
+/* ESERCIZIO 9 (EXTRA)
+  Scrivi una funzione per eliminare solo i valori PARI da un array.
+*/
+
+function eliminaPari(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
+}
+console.log(eliminaPari([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+/* ESERCIZIO 10 (EXTRA)
+  Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 10 (incluso), SENZA AMMETTERE DUPLICATI.
+ */
+
+function creaArrRandomNoDuplicati() {
+  let arr = [];
+  let numRandom;
+  let numero;
+  while (arr.length < 10) {
+    numRandom = Math.floor(Math.random() * 10);
+    //arr.push(numRandom);
+
+    let duplicato = false;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (numRandom === arr[i]) {
+        duplicato = true;
+        break;
+      }
+    }
+    if (!duplicato) {
+      arr.push(numRandom);
+    }
+  }
+  return arr;
+}
+console.log(creaArrRandomNoDuplicati());
+
+
+/* ESERCIZIO 11 (EXTRA)
+  Scrivi un algoritmo in grado di invertire un array.
+  es: [1, 3, 5] ==> [5, 3, 1]
+*/
+function invertiArray(array) {
+
+  array.reverse();
+  return array;
+}
+console.log(invertiArray([1, 3, 5]));
+
+/* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
+const movies = [
+  {
+    Title: 'The Lord of the Rings: The Fellowship of the Ring',
+    Year: '2001',
+    imdbID: 'tt0120737',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Return of the King',
+    Year: '2003',
+    imdbID: 'tt0167260',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Two Towers',
+    Year: '2002',
+    imdbID: 'tt0167261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of War',
+    Year: '2005',
+    imdbID: 'tt0399295',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lords of Dogtown',
+    Year: '2005',
+    imdbID: 'tt0355702',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings',
+    Year: '1978',
+    imdbID: 'tt0077869',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1990',
+    imdbID: 'tt0100054',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lords of Salem',
+    Year: '2012',
+    imdbID: 'tt1731697',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
+    Year: '1984',
+    imdbID: 'tt0087365',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1963',
+    imdbID: 'tt0057261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Avengers',
+    Year: '2012',
+    imdbID: 'tt0848228',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Infinity War',
+    Year: '2018',
+    imdbID: 'tt4154756',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Age of Ultron',
+    Year: '2015',
+    imdbID: 'tt2395427',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Endgame',
+    Year: '2019',
+    imdbID: 'tt4154796',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+  },
+]
+
+/* ESERCIZIO 12
+  Scrivi una funzione per trovare il film più vecchio nell'array fornito.
+*/
+function trovaFilmPiuVecchio(movies){
+  let olderFilm = movies[1];
+
+  for(i=1;i<movies.length;i++){
+    if(movies[i].Year<olderFilm.Year){
+      olderFilm=movies[i];
+    }
+  }
+  return olderFilm;
+
+}
+console.log(trovaFilmPiuVecchio(movies));
+
+/* ESERCIZIO 13
+  Scrivi una funzione per ottenere il numero di film contenuti nell'array fornito.
+*/
+function numeroFilmNellArray(movies) {
+
+  return movies.length;
+  
+}
+console.log(numeroFilmNellArray(movies));
+
+/* ESERCIZIO 14
+  Scrivi una funzione per creare un array con solamente i titoli dei film contenuti nell'array fornito.
+*/
+function creaArrayTitolo(movies) {
+  let titoli = [];
+  for(i=0;i<movies.length;i++){
+    titoli.push(movies[i].Title);
+  }
+  return titoli;
+}
+console.log(creaArrayTitolo(movies));
+
+/* ESERCIZIO 15
+  Scrivi una funzione per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
+*/
+
+/* ESERCIZIO 16
+  Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
+*/
+
+/* ESERCIZIO 17
+  Scrivi una funzione per calcolare la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array fornito.
+*/
+
+/* ESERCIZIO 18 (EXTRA)
+  Scrivi una funzione per recuperare tutti i film dall'array fornito che contengono una parola fornita.
+*/
