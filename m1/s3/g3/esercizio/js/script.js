7671fe451f88489eb69aeb25dcfce8702c9f87a7
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-02-16 09:30:39 +0100

    aggiunti commenti all'esercizio file js

--- m1/s3/g3/esercizio/js/script.js
+++ m1/s3/g3/esercizio/js/script.js
@@ -9,30 +9,30 @@ class Attivita {
 
     creaDiv() {
 
-        let div = document.createElement('div');
-        div.classList.add('task-item');
+        let div = document.createElement('div');//creo elemento div
+        div.classList.add('task-item');// aggiungo al div la classe task-item
 
-        let nomeAttivita = document.createElement('p');
-        nomeAttivita.textContent = this.nome;
+        let nomeAttivita = document.createElement('p'); //creo un paragrafo 
+        nomeAttivita.textContent = this.nome; // assegno il nome dell'Attivita al contenuto del paragrafo
 
-        div.appendChild(nomeAttivita);
+        div.appendChild(nomeAttivita); // l'elemento nomeAttivita (p) viene aggiunto all'interno del parent div.task-item
 
-        let divButton = document.createElement('div');
-        divButton.classList.add('button');
+        let divButton = document.createElement('div'); //creo un nuovo div
+        divButton.classList.add('button'); //assegno al div appena creato la classe button
         
-        div.appendChild(divButton)
+        div.appendChild(divButton) // il divButton viene aggiunto all'interno del parent div
 
-        let rimuoviButton = document.createElement('button');
-        rimuoviButton.textContent = '✘';
-        rimuoviButton.setAttribute('onclick', 'rimuoviTask(this.parentNode.parentNode)');
-        divButton.appendChild(rimuoviButton);
+        let rimuoviButton = document.createElement('button'); //creo il bottone per la funzione rimuoviTask
+        rimuoviButton.textContent = '✘'; //assegno il valore al bottone
+        rimuoviButton.setAttribute('onclick', 'rimuoviTask(this.parentNode.parentNode)'); // aggiungo proprieta al bottone (onClick). this.parentNode.parentNode viene utilizzato per accedere all'elemento "nonno" dell'elemento su cui viene chiamato il metodo rimuoviTask().
+        divButton.appendChild(rimuoviButton); //il bottone viene aggiunto all'interno del divButton
 
-        let completaButton = document.createElement('button');
-        completaButton.textContent = '✔';
-        completaButton.setAttribute('onclick', 'completaTask(this.parentNode.parentNode)');
-        divButton.appendChild(completaButton);
+        let completaButton = document.createElement('button'); // creo un nuovo bottone completaButton
+        completaButton.textContent = '✔';// assegno il valore al bottone
+        completaButton.setAttribute('onclick', 'completaTask(this.parentNode.parentNode)'); //stesso concetto di rimuoviButton
+        divButton.appendChild(completaButton); //inserisco il bottone all'interno del div button
 
-        document.querySelector('.task-list').appendChild(div);
+        document.querySelector('.task-list').appendChild(div); // il div con classe task-item creato in linea 13 è figlio del div task-list
     }
 }
 
@@ -58,11 +58,11 @@ function rimuoviTask(div) {
 }
 function completaTask(divButton) {
     for (let task of arrTask) {
-        if (divButton.textContent.includes(task.nome)) {
-            if(!task.isCompleta){
+        if (divButton.textContent.includes(task.nome)) { 
+            if(!task.isCompleta){ //se task.isCompleta == false
                 task.isCompleta = true;
                 divButton.querySelector('p').style.textDecoration = 'line-through';
-            }else{
+            }else{ // altrimenti se è gia true e ho sbagliato perchè l'attività non l'ho ancora svolta 
                 task.isCompleta=false;
                 divButton.querySelector('p').style.textDecoration = 'none';
             }
